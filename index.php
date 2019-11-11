a5236866658450c10e512cbb7bed34c981d8fa89
emily <retyma24@gmail.com>
2019-11-11 16:44:46 +0100

    aggiornamento
    aggiornamento

--- index.php
+++ index.php
@@ -23,18 +23,26 @@ session_start();
     </head>
     <body>
         <div class="background"></div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-            <a class="navbar-brand" href="">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-black sticky-top">
+            <!-- Image and text -->
+            <a class="navbar-brand" href="index.php">
                 <img src="img/logoITT.png" width="30" height="30" class="d-inline-block align-top" alt="">
-                ITT terremoti
+                ITT Terni
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="pages/terremoti.html">Terremoti</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+                            Terremoti
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="pages/terremoti_in_italia.html">In Italia</a>
+                            <a class="dropdown-item" href="pages/terremoti_nel_mondo.html">Nel mondo</a>
+                            <a class="dropdown-item" href="pages/terremoti_nel_tempo.html">Nel tempo</a>
+                        </div>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="pages/zone.html">Zone</a>
@@ -42,16 +50,18 @@ session_start();
                     <li class="nav-item">
                         <a class="nav-link" href="pages/faqs.html">FAQs</a>
                     </li>
-                    <?php if (isset($_SESSION['id'])) { ?>
-                        <li class="nav-item">
-                            <a class="nav-link text-info" href="logout.php"><i class="fa fa-sign-out"></i> Logout</a>
-                        </li>
-                    <?php } else { ?>
-                        <li class="nav-item">
-                            <a class="nav-link text-danger" href="login.php"><i class="fa fa-sign-in"></i> Login</a>
-                        </li>
-                    <?php } ?>
                 </ul>
+                <form class="form-inline my-2 my-lg-0" method="get" action="search.php">
+                    <div class="btn-group input-group my-2">
+                        <input class="form-control" type="search" name="q" placeholder="Cerca">
+                        <button class="btn btn-secondary" type="submit"><i class="fa fa-search"></i> Cerca</button>
+                    </div>
+                    <?php if (isset($_SESSION['id'])) { ?>
+                        <a class="btn btn-outline-danger ml-sm-2" href="logout.php"><i class="fa fa-user"></i> Nome utente</a>
+                    <?php } else { ?>
+                        <a class="btn btn-danger ml-sm-2" href="login.php"><i class="fa fa-sign-in"></i> Login</a>
+                    <?php } ?>
+                </form>
             </div>
         </nav>
         <div align="center">
