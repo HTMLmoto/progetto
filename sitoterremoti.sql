e74a24f3183f0ea01413fcbf531b4b04a31dfee8
emily <retyma24@gmail.com>
2019-12-16 16:15:42 +0100

    1

--- sitoterremoti.sql
+++ sitoterremoti.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 02, 2019 alle 15:00
--- Versione del server: 5.6.15-log
--- Versione PHP: 5.5.8
+-- Creato il: Dic 16, 2019 alle 16:13
+-- Versione del server: 10.4.8-MariaDB
+-- Versione PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,9 +32,9 @@ CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `titolo` varchar(255) COLLATE utf8_bin NOT NULL,
   `contenuto` text COLLATE utf8_bin NOT NULL,
-  `data` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `pubblico` tinyint(4) NOT NULL DEFAULT '0',
-  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `data` datetime NOT NULL DEFAULT current_timestamp(),
+  `pubblico` tinyint(4) NOT NULL DEFAULT 0,
+  `deleted` tinyint(4) NOT NULL DEFAULT 0,
   `idUser` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -43,8 +43,7 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `titolo`, `contenuto`, `data`, `pubblico`, `deleted`, `idUser`) VALUES
-(19, 'Primo post', 'Contenuto primo post', '2019-11-18 16:01:40', 1, 0, 5),
-(20, 'terremoto', 'TU non hai scritto niente', '2019-11-18 16:26:18', 1, 0, 4);
+(1, 'Ultimo terremoto', 'Ultimo terremoto registrato in Peru di Magnitudo 6.9', '0000-00-00 00:00:00', 1, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -55,7 +54,6 @@ INSERT INTO `posts` (`id`, `titolo`, `contenuto`, `data`, `pubblico`, `deleted`,
 CREATE TABLE `utenti` (
   `id` int(11) NOT NULL,
   `nickname` varchar(255) COLLATE utf8_bin NOT NULL,
-  `email` varchar(255) COLLATE utf8_bin NOT NULL,
   `password` varchar(255) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -63,10 +61,15 @@ CREATE TABLE `utenti` (
 -- Dump dei dati per la tabella `utenti`
 --
 
-INSERT INTO `utenti` (`id`, `nickname`, `email`, `password`) VALUES
-(4, 'retyma24', 'retyma24@gmail.com', '01b9622ff3ce2d88484089505fa99a63'),
-(5, 'andrea 9', '1@1.com', '4b79f8dcb866dd99109347bc30b41fe6'),
-(6, '404', 'emiliotulli03@gmail.com', '189bbbb00c5f1fb7fba9ad9285f193d1');
+INSERT INTO `utenti` (`id`, `nickname`, `password`) VALUES
+(2, '404', '189bbbb00c5f1fb7fba9ad9285f193d1'),
+(3, 'Eyren', '4fd68417f607626bcfe4bb9a0eedc2a9'),
+(4, 'Angelo', '4d13a053ad2208574a5d50797be4bd2c'),
+(5, 'Antonio', 'bcb7e9e73ce1771a23777b05bd8d3ea9'),
+(6, 'manuz', '27e74a7187d63ee3fd2d4788efc98561'),
+(7, 'chaper', 'ccd8659a8b6a1b93d5ac6043d5ba9c19'),
+(8, 'Andrea 9', 'cf4fe3bb576c98af8f4f7127d18c6833'),
+(9, 'aurora', '99c8ef576f385bc322564d5694df6fc2');
 
 --
 -- Indici per le tabelle scaricate
@@ -93,23 +96,13 @@ ALTER TABLE `utenti`
 -- AUTO_INCREMENT per la tabella `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT per la tabella `utenti`
 --
 ALTER TABLE `utenti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- Limiti per le tabelle scaricate
---
-
---
--- Limiti per la tabella `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `utenti` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
