14448c9bd843b5b98749cf570ccbd7b725373564
emily <retyma24@gmail.com>
2019-12-16 16:32:48 +0100

    1

--- Libs/js/my-project.js
+++ Libs/js/my-project.js
@@ -1,11 +1,11 @@
  $(document).ready(function() {
 	$('.pop').popover({
-		placement: 'top',
-		html: true,
-		content: '<div class="row"><div class="col-sm-6 col-xs-12">Barbieri Andrea</div><div class="col-sm-6 col-xs-12">Tulli Emilio</div</div<div class="row"><div class="col-sm-6 col-xs-12">Mastrapasqua Simone (chap)</div><div class="col-sm-6 col-xs-12">Duti Antonio</div</div<div class="row"><div class="col-sm-6 col-xs-12">Di Giacomo Od Esposto Diego</div><div class="col-sm-6 col-xs-12">Dinica Angelo Ilie</div</div<div class="row"><div class="col-sm-6 col-xs-12">Kozak Anatoliy</div</div<div class="row"><div class="col-sm-6 col-xs-12">Franceschini Lorenzo</div><div class="col-sm-6 col-xs-12">Rosetti Aurora</div</div<div class="row"><div class="col-sm-6 col-xs-12">Della Ciana Emiliano</div><div class="col-sm-6 col-xs-12">Prodanchuk Vladyslav</div</div>',
+		placement:'top',
+		html:true,
+		content:'<div class="row"><div class="col-sm-6 col-xs-12">Barbieri Andrea</div><div class="col-sm-6 col-xs-12">Tulli Emilio</div></div><div class="row"><div class="col-sm-6 col-xs-12">Mastrapasqua Simone (chap)</div><div class="col-sm-6 col-xs-12">Duti Antonio</div></div><div class="row"><div class="col-sm-6 col-xs-12">Di Giacomo Od Esposto Diego</div><div class="col-sm-6 col-xs-12">Dinica Angelo Ilie</div></div><div class="row"><div class="col-sm-6 col-xs-12">Kozak Anatoliy</div><div class="col-sm-6 col-xs-12">Rosetti Aurora</div></div><div class="row"><div class="col-sm-6 col-xs-12">Franceschini Lorenzo</div><div class="col-sm-6 col-xs-12">Prodanchuk Vladyslav</div></div><div class="row"><div class="col-sm-6 col-xs-12">Della Ciana Emiliano</div></div>',
 		template:'<div class="popover my-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body my-popover-body"></div></div>',
 	});
     $('.carousel').carousel({
-        interval: 4000
+      interval: 4000
     });
 });
